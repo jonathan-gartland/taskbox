a00aefce7cee56a9e9ceeddcdba4a698ee148ba6
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-17 22:58:59 -0500

    maybe deploy? trying with npm

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,31 @@
 import '../src/index.css';
-
+import { themes } from '@storybook/theming';
 // Registers the msw addon
-import { initialize, mswLoader } from 'msw-storybook-addon';
+import { initialize, mswLoader, mswDecorator } from 'msw-storybook-addon';
 
 // Initialize MSW
-initialize();
 
+let options = {};
+if (location.hostname === "jonathan-gartland.github.io") {
+  options = {
+    serviceWorker: {
+      url: "/taskbox/mockServiceWorker.js"
+    }
+  }
+}
+
+// Initialize MSW
+initialize({
+  // Remove log das requisições não tratadas
+  onUnhandledRequest: 'bypass',
+  ...options
+});
+
+export const decorators = [mswDecorator];
 //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+
   parameters: {
     controls: {
       matchers: {
